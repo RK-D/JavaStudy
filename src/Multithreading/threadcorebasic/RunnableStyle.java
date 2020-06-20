f44bf9c352d5b2e26c6723855232b8d1f0fa9882
RK-D <3245885483@qq.com>
2020-06-20 10:42:53 +0800

    JavaStudy

--- src/Multithreading/threadcorebasic/RunnableStyle.java
+++ src/Multithreading/threadcorebasic/RunnableStyle.java
@@ -4,6 +4,15 @@ package Multithreading.threadcorebasic;
  * @author rookie
  * @version 1.0
  * @date 2020/6/20 10:31
+ * Runnable方式创建线程
  */
-public class RunnableStyle {
+public class RunnableStyle implements Runnable {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new RunnableStyle());
+        thread.start();
+    }
+    @Override
+    public void run() {
+        System.out.println("Runnable多线程");
+    }
 }
